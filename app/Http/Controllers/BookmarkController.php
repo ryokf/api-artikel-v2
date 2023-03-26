5fce61f85b6d01f89db3e93d3e83ded35a11f4c7
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-03-26 20:21:42 +0700

    fix comment controller selesai

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -6,49 +6,49 @@ use app\Helpers\ResponseFormatter;
 use App\Http\Resources\BookmarkDetailResource;
 use App\Models\Article;
 use App\Models\Bookmark;
-use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 
 class BookmarkController extends Controller
 {
     function index(Request $request){
-        if($request->user_id == null){
+        if($request->user()->id == null){
             return ResponseFormatter::response(404, 'error', "anda belum login");
         }
 
-        $bookmark = Bookmark::where('user_id', $request->user_id)->paginate(20);
+        $bookmark = Bookmark::where('user_id', $request->user()->id)->paginate(20);
 
         return ResponseFormatter::response(200, 'success', BookmarkDetailResource::collection($bookmark));
     }
 
     function store(Request $request){
-        if($request->user_id == null){
+        if($request->user()->id == null){
             return ResponseFormatter::response(404, 'error', "anda belum login");
         }
+
         $article = Article::where('id', $request->article_id)?->first() ?? false;
 
         if(!$article){
             return ResponseFormatter::response(404, 'error', "artikel tidak ada");
         }
 
-        $bookmarkAdded = Bookmark::where('article_id', $request->article_id)->where('user_id', $request->user_id)?->first() ?? false;
+        $bookmarkAdded = Bookmark::where('article_id', $request->article_id)->where('user_id', $request->user()->id)?->first() ?? false;
 
         if($bookmarkAdded){
             return ResponseFormatter::response(404, 'error', "Bookmark sudah ada");
         }
 
         Bookmark::create([
-            'user_id' => $request->user_id,
+            'user_id' => $request->user()->id,
             'article_id' => $request->article_id
         ]);
 
-        $bookmark = Bookmark::where('user_id', $request->user_id)->where('article_id', $request->article_id)->first();
+        $bookmark = Bookmark::where('user_id', $request->user()->id)->where('article_id', $request->article_id)->first();
 
         return ResponseFormatter::response(200, 'success', new BookmarkDetailResource($bookmark));
     }
 
     function destroy(Request $request){
-        if($request->user_id == null){
+        if($request->user()->id == null){
             return ResponseFormatter::response(404, 'error', "anda belum login");
         }
         $bookmark =  Bookmark::where('id',$request->id)->first() ?? false;
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -11,12 +11,11 @@ class CommentController extends Controller
     function store(Request $request){
         $request->validate([
             'article_id' => 'required',
-            'user_id' => 'required',
             'comment' => 'required|max:255',
         ]);
 
         $commentData = [
-            'user_id' => $request->user_id,
+            'user_id' => $request->user()->id,
             'article_id' => $request->article_id,
             'comment' => $request->comment
         ];
@@ -38,6 +37,16 @@ class CommentController extends Controller
             'updated' => true
         ];
 
+        $userId = Comment::where('id', $request->id)->first()->user_id;
+
+        if ($userId != $request->user()->id) {
+            return ResponseFormatter::response(
+                404,
+                'error',
+                'anda tidak terautentikasi'
+            );
+        }
+
         Comment::where('id', $request->id)->update($commentData);
 
         return ResponseFormatter::response(200, 'success', $commentData);
@@ -50,7 +59,18 @@ class CommentController extends Controller
             return ResponseFormatter::response(404, 'error', "comment tidak ada");
         }
 
+        $userId = Comment::where('id', $request->id)->first()->user_id;
+
+        if ($userId != $request->user()->id) {
+            return ResponseFormatter::response(
+                404,
+                'error',
+                'anda tidak terautentikasi'
+            );
+        }
+
         Comment::where('id', $request->id)->delete();
+
         return ResponseFormatter::response(200, 'success', $comment);
     }
 }
