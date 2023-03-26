0aede7f8a3ea2b944afb4b2b00bec40b0dd4bd03
Ryo Khrisna Fitriawan <ryokhrisnaf@gmail.com>
2023-03-26 20:26:36 +0700

    fix user interest controller selesai

--- app/Http/Controllers/UserInterestController.php
+++ app/Http/Controllers/UserInterestController.php
@@ -10,14 +10,14 @@ class UserInterestController extends Controller
 {
     function store(Request $request)
     {
-        $userInterest = UserInterest::where('category_id', $request->category_id)?->where('user_id', $request->user_id)?->first() ?? false;
+        $userInterest = UserInterest::where('category_id', $request->category_id)?->where('user_id', $request->user()->id)?->first() ?? false;
 
         if ($userInterest) {
             return ResponseFormatter::response(404, 'error', "user interest sudah ada");
         }
 
         $userInterestData = [
-            'user_id' => $request->user_id,
+            'user_id' => $request->user()->id,
             'category_id' => $request->category_id,
         ];
 
@@ -25,15 +25,26 @@ class UserInterestController extends Controller
 
         return ResponseFormatter::response(200, 'success', $userInterestData);
     }
+
     function destroy(Request $request)
     {
-        $userInterest = UserInterest::where('category_id', $request->category_id)?->where('user_id', $request->user_id)?->first() ?? false;
+        $userInterest = UserInterest::where('id', $request->id)?->first() ?? false;
 
         if (!$userInterest) {
             return ResponseFormatter::response(404, 'error', "user interest tidak ada");
         }
 
-        UserInterest::where('category_id', $request->category_id)?->where('user_id', $request->user_id)?->delete();
+        $userId = UserInterest::where('id', $request->id)->first()->user_id;
+
+        if ($userId != $request->user()->id) {
+            return ResponseFormatter::response(
+                404,
+                'error',
+                'anda tidak terautentikasi'
+            );
+        }
+
+        UserInterest::where('id', $request->id)->delete();
 
         return ResponseFormatter::response(200, 'success', $userInterest);
     }
